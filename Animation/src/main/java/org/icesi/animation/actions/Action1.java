6dd3075d3f10e0f4ee9b6ad05eaa66bb9708dd78
Tomas Jaramillo Caicedo <1032876581@u.icesi.edu.co>
2025-05-16 09:14:47 -0500

    Pequeña correcion de clase pasada

--- Animation/src/main/java/org/icesi/animation/actions/Action1.java
+++ Animation/src/main/java/org/icesi/animation/actions/Action1.java
@@ -18,12 +18,14 @@ public class Action1 implements Runnable{
     // Notify subscribers
     @Override
     public void run() {
-        try{
-
-            subscriber.setX(subscriber.getX() + 2);
-            Thread.sleep(subscriber.getTimer());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while (subscriber.isAlive()){
+            try{
+                subscriber.setY(subscriber.getY() + 2);
+                Thread.sleep(subscriber.getTimer());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+
     }
 }
